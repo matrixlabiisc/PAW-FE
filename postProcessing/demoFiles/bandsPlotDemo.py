8585bacff3936465eef62886d4903d91f436dff0
Srinibas Nandi <srinibasn@iisc.ac.in>
2024-03-06 17:48:53 +0530

    Bandstructure and DOS plot functions added

--- postProcessing/demoFiles/bandsPlotDemo.py
+++ postProcessing/demoFiles/bandsPlotDemo.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.append("/home/srinibasnandi/project_works/bandsPR/dftfe") # append the path of DFTFE to sys.path
 
-from postProcessing.plotBandstructure import plotBandStr
-
-plotBandStr("./","bands.out", "kpointRuleFile.inp", "coordinatesRelaxed.inp", "domainVectorsRelaxed.inp", [-14.0, -5.0], True)
+from postProcessing.postprocessModules import Plotters
+plotter = Plotters("./","bands.out", "kpointRuleFile.inp", "coordinatesRelaxed.inp", "domainVectorsRelaxed.inp","pseudo.inp","dosData.out", [-14.0, -5.0], True)
+# plotter = Plotters("./","bands.out", "kpointRuleFile.inp", "coordinatesRelaxed.inp", "domainVectorsRelaxed.inp","pseudo.inp","dosData.out", [-20.0, 20.0], True)
+plotter.plotBandStr()
+plotter.plotDos()
