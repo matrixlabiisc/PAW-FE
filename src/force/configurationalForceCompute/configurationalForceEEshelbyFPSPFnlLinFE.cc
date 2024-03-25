9f5c431a0450d31dde8bfe029499e1c17056f313
Sambit Das <dsambit@umich.edu>
2024-03-25 10:08:59 -0400

    all electron force fix

--- src/force/configurationalForceCompute/configurationalForceEEshelbyFPSPFnlLinFE.cc
+++ src/force/configurationalForceCompute/configurationalForceEEshelbyFPSPFnlLinFE.cc
@@ -263,12 +263,10 @@ namespace dftfe
               numPhysicalCells,
               numQuadPoints,
               numQuadPointsNLP,
-              &elocWfcEshelbyTensorQuadValuesH[0],
-              &projectorKetTimesPsiTimesVTimesPartOccContractionGradPsiQuadsFlattened
-                [0],
+              elocWfcEshelbyTensorQuadValuesH.data(),
+              projectorKetTimesPsiTimesVTimesPartOccContractionGradPsiQuadsFlattened.data(),
 #  ifdef USE_COMPLEX
-              &projectorKetTimesPsiTimesVTimesPartOccContractionPsiQuadsFlattened
-                [0],
+              projectorKetTimesPsiTimesVTimesPartOccContractionPsiQuadsFlattened.data(),
 #  endif
               d_mpiCommParent,
               dftPtr->interBandGroupComm,
@@ -306,12 +304,10 @@ namespace dftfe
               numPhysicalCells,
               numQuadPoints,
               numQuadPointsNLP,
-              &elocWfcEshelbyTensorQuadValuesH[0],
-              &projectorKetTimesPsiTimesVTimesPartOccContractionGradPsiQuadsFlattened
-                [0],
+              elocWfcEshelbyTensorQuadValuesH.data(),
+              projectorKetTimesPsiTimesVTimesPartOccContractionGradPsiQuadsFlattened.data(),
 #ifdef USE_COMPLEX
-              &projectorKetTimesPsiTimesVTimesPartOccContractionPsiQuadsFlattened
-                [0],
+              projectorKetTimesPsiTimesVTimesPartOccContractionPsiQuadsFlattened.data(),
 #endif
               d_mpiCommParent,
               dftPtr->interBandGroupComm,
