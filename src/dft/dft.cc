8e351c46b8b857d2d0f4100f8dfd854a9c230f7e
Kartick Ramakrishnan <kartickr@iisc.ac.in>
2024-06-07 22:28:35 +0530

    PsiInitGuess changes to prevent underflow

--- src/dft/dft.cc
+++ src/dft/dft.cc
@@ -2783,8 +2783,7 @@ namespace dftfe
                     if (d_dftParamsPtr->useGradPhiMixing)
                       d_mixingScheme.computeAndersonMixingCoeff(
                         std::vector<mixingVariable>{mixingVariable::gradPhi,
-                                                    mixingVariable::rho,
-                                                    mixingVariable::DijMatrix});
+                                                    mixingVariable::rho});
                     else
                       d_mixingScheme.computeAndersonMixingCoeff(
                         d_dftParamsPtr->spinPolarized == 1 ?
